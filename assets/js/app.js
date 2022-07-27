4f7ae97480489435f3395ae05df887dc20a5d7a0
Julio Pinto <pinto.julioj03@gmail.com>
2022-07-27 09:57:47 +0100

    feat: csrfToken

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,6 +53,10 @@ Hooks.CustomSelect = {
   },
 };
 
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+  
 let liveSocket = new LiveSocket("/live", Socket, {
     params: { _csrf_token: csrfToken },
     hooks: Hooks,
