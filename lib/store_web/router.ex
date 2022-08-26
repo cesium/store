ef21d9fc5bdddf36eee5274c073a6359a31f6bc3
MarioRodrigues10 <mjsrodrigues4@gmail.com>
2022-08-26 17:37:44 +0100

    fix errors

--- lib/store_web/router.ex
+++ lib/store_web/router.ex
@@ -22,6 +22,7 @@ defmodule StoreWeb.Router do
 
     live "/", HomeLive.Index, :index
 
+
     live "/products", ProductLive.Index, :index
     live "/products/new", ProductLive.Index, :new
     live "/products/:id/edit", ProductLive.Index, :edit
