1a6553a4edb28cec7314a015ff1ca7fc7e4d15fd
Mário Rodrigues <mjsrodrigues4@gmail.com>
2022-12-05 08:49:43 +0000

    change mix test

--- mix.exs
+++ mix.exs
@@ -72,7 +72,7 @@ defmodule Store.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.dump", "ecto.load --quiet --skip-if-loaded", "test"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
