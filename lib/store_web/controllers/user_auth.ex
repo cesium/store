de4b843bb1c25532dae4984b53fe458758f72fb8
Mário Rodrigues <mjsrodrigues4@gmail.com>
2022-12-07 16:17:19 +0000

    fix warning

--- lib/store_web/controllers/user_auth.ex
+++ lib/store_web/controllers/user_auth.ex
@@ -134,7 +134,7 @@ defmodule StoreWeb.UserAuth do
       conn
       |> put_flash(:error, "You must log in to access this page.")
       |> maybe_store_return_to()
-      |> redirect(to: "/users/log_in")
+      |> redirect(to: Routes.user_session_path(conn, :new))
       |> halt()
     end
   end
