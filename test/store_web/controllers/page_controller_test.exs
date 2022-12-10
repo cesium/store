e752aab3cd74302a9bc0a9b9f0ae6e5b91a37316
Daniel Pereira <46000449+danielsp45@users.noreply.github.com>
2022-12-10 20:56:13 +0000

    Add landing page (#38)
    * cart
    
    * resolve commits
    
    * resolve commits
    
    * resolve mistakes
    
    * solve purchase bug && add delete product from order
    
    * solve errors
    
    * solve mistakes
    
    * solve current_user errors
    
    * add snapp scroll
    
    * small fix
    
    * add snapp scroll horizontal
    
    * add carousel slider
    
    * Start store main page
    
    * update main page
    
    * add updates
    
    * add main page
    
    * run formatter
    
    * fix action
    
    * fix warning
    
    * fix scroll
    
    Co-authored-by: MarioRodrigues10 <mjsrodrigues4@gmail.com>
    Co-authored-by: Mário Rodrigues <93675410+MarioRodrigues10@users.noreply.github.com>

--- test/store_web/controllers/page_controller_test.exs
+++ test/store_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule StoreWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix"
+    assert html_response(conn, 200) =~ "Buy Now"
   end
 end
