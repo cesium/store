ca9997e4cacdf2e4e169770ac7f0ece8e8f94118
João Lobo <joaodlobo@gmail.com>
2023-03-13 22:58:07 +0000

    Add confirmation modal to product edit page

--- lib/store_web/live/live_helpers.ex
+++ lib/store_web/live/live_helpers.ex
@@ -27,10 +27,10 @@ defmodule StoreWeb.LiveHelpers do
     assigns = assign_new(assigns, :return_to, fn -> nil end)
 
     ~H"""
-    <div id="modal" class="phx-modal fade-in" phx-remove={hide_modal()}>
+    <div id="modal" class="phx-modal fade-in hidden" phx-remove={hide_modal()}>
       <div
         id="modal-content"
-        class="phx-modal-content fade-in-scale"
+        class="phx-modal-content fade-in-scale rounded-xl max-w-md"
         phx-click-away={JS.dispatch("click", to: "#close")}
         phx-window-keydown={JS.dispatch("click", to: "#close")}
         phx-key="escape"
@@ -43,7 +43,10 @@ defmodule StoreWeb.LiveHelpers do
             phx_click: hide_modal()
           ) %>
         <% else %>
-          <a id="close" href="#" class="phx-modal-close" phx-click={hide_modal()}>✖</a>
+          <button id="close" type="button" phx-click={hide_modal()} class="float-right top-3 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="popup-modal">
+            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <span class="sr-only">Close modal</span>
+          </button>
         <% end %>
 
         <%= render_slot(@inner_block) %>
@@ -52,9 +55,15 @@ defmodule StoreWeb.LiveHelpers do
     """
   end
 
-  defp hide_modal(js \\ %JS{}) do
+  def hide_modal(js \\ %JS{}) do
     js
     |> JS.hide(to: "#modal", transition: "fade-out")
     |> JS.hide(to: "#modal-content", transition: "fade-out-scale")
+  end
+
+  def show_modal(js \\ %JS{}) do
+    js
+    |> JS.show(to: "#modal", transition: "fade-in")
+    |> JS.show(to: "#modal-content", transition: "fade-in-scale")
   end
 end
